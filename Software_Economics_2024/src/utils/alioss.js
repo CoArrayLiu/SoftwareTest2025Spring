99f63244e7895fda1d22213f8462fe7ed97f7434
CoArrayLiu <3061632379@qq.com>
2025-06-10 19:26:35 +0800

    ST

--- Software_Economics_2024/src/utils/alioss.js
+++ Software_Economics_2024/src/utils/alioss.js
@@ -4,9 +4,5 @@ import OSS from 'ali-oss';
 
 export function client() {
   return new OSS({
-    region: '', // 注意：这里的 URL 不需要包含 'aliyuncs.com'
-    accessKeyId: '',
-    accessKeySecret: '',
-    bucket: ''
   });
 }
